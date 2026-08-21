7020b522f9f6d37495cab7073d6342103c0882b9
Bhushan Kampelwar <bkamplewar@gmail.com>
2026-08-21 00:34:21 -0600

    Update project

--- src/main/java/com/calley/pages/StandardImportPage.java
+++ src/main/java/com/calley/pages/StandardImportPage.java
@@ -1,5 +1,25 @@
 package com.calley.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class StandardImportPage {
 
+    WebDriver driver;
+
+    // Constructor
+    public StandardImportPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //locater add standard import
+    By standardImport = By.cssSelector(
+    		"li[class='active'] li:nth-child(1) a:nth-child(1) span:nth-child(1)"
+    		
+    );
+    
+    // action 
+    public void ClickStandardImport() {
+    	driver.findElement(standardImport);
+    }
 }
--- src/test/java/com/calley/tests/StandardImportTest.java
+++ src/test/java/com/calley/tests/StandardImportTest.java
@@ -1,5 +1,30 @@
 package com.calley.tests;
 
-public class StandardImportTest {
+import org.testng.annotations.Test;
 
+import com.calley.base.BaseTest;
+import com.calley.pages.LoginPage;
+import com.calley.pages.StandardImportPage;
+
+public class StandardImportTest extends BaseTest {
+
+    @Test
+    public void openStandardImport() {
+    	
+    	//for loging
+    	
+    	LoginPage loginPage = new LoginPage(driver);
+    	loginPage.login("zas@mailinator.com", "Pa$$w0rd!");
+    	
+    	System.out.println(" Login Successful ");
+    	
+    	// Standard import
+    	
+        StandardImportPage standardImport = new StandardImportPage(driver);
+
+        standardImport.ClickStandardImport();
+        
+        System.out.println("Standard Import Page Test Started");
+
+    }
 }
