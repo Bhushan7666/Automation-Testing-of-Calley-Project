20b22a6efcfa3fb308943a918346e57276ad11d8
Bhushan Kampelwar <bkamplewar@gmail.com>
2026-08-30 23:29:25 -0600

    done

--- src/main/java/com/calley/pages/StandardImportPage.java
+++ src/main/java/com/calley/pages/StandardImportPage.java
@@ -1,8 +1,12 @@
 package com.calley.pages;
 
-
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class StandardImportPage {
 
@@ -13,26 +17,34 @@ public class StandardImportPage {
         this.driver = driver;
     }
 
-    //locater add standard import
-    By standardImport = By.cssSelector(
-    		"li[class='active'] li:nth-child(1) a:nth-child(1) span:nth-child(1)"		
-    );
+    // Correct Locators
+    By callListMenu = By.xpath("//a[normalize-space()='Call List']");
+    By standardImport = By.xpath("//a[contains(text(),'Standard Import')]");
+    By listName = By.cssSelector("#ContentPlaceHolder1_txtlistname");
+    By fileUpload = By.cssSelector("input[type='file']");									
     
-   // By listName = By.cssSelector("  ");
-    
-    // File Upload
-   /// By fileUpload = By.cssSelector("  ");
-    
-    // action 
+    // Actions
     public void ClickStandardImport() {
-    	driver.findElement(standardImport);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        
+        // 1. Call List visible hone tak wait karein
+        WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(callListMenu));
+        
+        // 2. Call List par Hover karein
+        Actions actions = new Actions(driver);
+        actions.moveToElement(menu).perform();
+        
+        // 3. Sub-menu (Standard Import) clickable hone par click karein
+        WebElement subMenu = wait.until(ExpectedConditions.elementToBeClickable(standardImport));
+        subMenu.click();
     }
     
     public void enterListName(String name) {
- //   	driver.findElement(listName).sendKeys(name);
-    }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(listName)).sendKeys(name);
+    } 
 
     public void uploadFile(String filePath) {
- //       driver.findElement(fileUpload).sendKeys(filePath);
+        driver.findElement(fileUpload).sendKeys(filePath);
     }
 }
